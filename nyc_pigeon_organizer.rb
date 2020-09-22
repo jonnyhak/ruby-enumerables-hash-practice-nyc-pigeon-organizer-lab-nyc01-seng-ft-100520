cf65a65c7fc5fe013f0081c429b2fe788868a449
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-22 14:38:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+
+  data.each do |outer_key, sub_hash|
+    sub_hash.each do |inner_key, name|
+      if hash[name]
+        hash[name][outer_key] << inner_key
+      else
+        hash[name] = {outer_key => [inner_key]}
+      end
+    end
+    
+  end 
+  hash
 end
